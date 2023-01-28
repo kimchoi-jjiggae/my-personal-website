9fe9828bb4987d8c5c2131e9c6d3d2c852f281a5
Michelle <choi.michelle.12@gmail.com>
2023-01-28 15:04:53 -0500

    some fixes to nav bar

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // const h2 = document.createElement("h2");
 // h2.textContent = "This content added by JavaScript";
 // document.querySelector("body").appendChild(h2);
+
+const toggleButton = document.getElementsByClassName(`toggle-button`)[0]
+const navBarLinks = document.getElementsByClassName(`navBar-links`)[0]
+
+toggleButton.addEventListener(`click`, function () {
+    navBarLinks.classList.toggle(`active`)
+})
